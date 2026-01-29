82bc92d58539b987d0eebf72e12e62ba065e7408
Andris011 <andris.febert111@gmail.com>
2026-01-29 14:33:26 +0100

    Create CaseStatus.cs

---
+++ CaseStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace _29_01_2026_DigitalisNyomozas;
+
+public class CaseStatus
+{
+    private string status;
+
+    public CaseStatus(string status)
+    {
+        this.status = status;
+    }
+
+    public string Status
+    {
+        get => status;
+        set => status = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
