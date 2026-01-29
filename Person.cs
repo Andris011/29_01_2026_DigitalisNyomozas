dc787ad9f45c061db7eb26f3a7702fc280695afc
Andris011 <andris.febert111@gmail.com>
2026-01-29 14:35:51 +0100

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿namespace _29_01_2026_DigitalisNyomozas;
+
+public class Person
+{
+    private string nev;
+    private int eletkor;
+    private string megjegyzes;
+
+    public Person(string nev, int eletkor, string megjegyzes)
+    {
+        this.nev = nev;
+        this.eletkor = eletkor;
+        this.megjegyzes = megjegyzes;
+    }
+
+    public string Nev
+    {
+        get => nev;
+        set => nev = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Eletkor
+    {
+        get => eletkor;
+        set => eletkor = value;
+    }
+
+    public string Megjegyzes
+    {
+        get => megjegyzes;
+        set => megjegyzes = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
