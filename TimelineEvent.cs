5b68976bca62d4cd99446572587de3de0e58ffd7
Andris011 <andris.febert111@gmail.com>
2026-01-29 14:48:09 +0100

    Create TimelineEvent.cs

---
+++ TimelineEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace _29_01_2026_DigitalisNyomozas;
+
+public class TimelineEvent
+{
+    private string datum;
+    private string leiras;
+
+    public TimelineEvent(string datum, string leiras)
+    {
+        this.datum = datum;
+        this.leiras = leiras;
+    }
+
+    public string Datum
+    {
+        get => datum;
+        set => datum = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Leiras
+    {
+        get => leiras;
+        set => leiras = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
